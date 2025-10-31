b01079d39491b2ccc02ef3de8b7288f2bfb44d1b
t123arun <tarunkamboj49@gmail.com>
2025-10-31 12:49:53 +0530

    improved accessibility code

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,60 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [result] = useState(null);
 
   const handleCalculate = () => {};
 
   return (
-    <main style={{ padding: '20px', backgroundColor: '#fff', color: '#222' }} aria-labelledby='app-title'>
+    <main
+      style={{ padding: "20px", backgroundColor: "#fff", color: "#222" }}
+      aria-labelledby="app-title"
+    >
       <header>
-        <h1 id='app-title'>String Calculator</h1>
+        <h1 id="app-title">String Calculator</h1>
       </header>
       <img
-        src='https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        src="https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         width={600}
         height={400}
-        alt='A white yarn on white surface'
+        alt="A white yarn on white surface"
       />
 
-      <h2 style={{ fontSize: '20px' }}>Enter numbers</h2>
-      
-    <label htmlFor='numbers-input'>
-      Input Number
-    </label>
-      <textarea
-       id='numbers-input'
-        style={{ margin: '10px 0', color: '#aaa' }}
-        name='numbers-input'
-        placeholder='Enter numbers'
-        aria-describedby="Enter numbers"
-        aria-required="true"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
+      <h2 style={{ fontSize: "20px" }}>Enter numbers</h2>
 
-      <button
-       aria-label="handle calculate button"
-        onClick={handleCalculate}
-        style={{
-          padding: '10px',
-          backgroundColor: '#0078D4',
-          color: '#fff',
-          border: 'none',
-        }}>
-        Calculate
-      </button>
+        <label htmlFor="numbers-input">Input Number</label>
+        <textarea
+          id="numbers-input"
+          style={{ margin: "10px 0", color: "#555" }}
+          name="numbers-input"
+          placeholder="Enter numbers"
+          aria-required="true"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          type="button"
+          aria-label="Calculate result"
+          onClick={handleCalculate}
+          style={{
+            padding: "10px",
+            backgroundColor: "#0078D4",
+            color: "#fff",
+            border: "none",
+          }}
+        >
+          Calculate
+        </button>
 
-      {result !== null && <p  aria-live="polite" style={{ color: 'green' }}>Result: {result}</p>}
+      {result !== null && (
+        <p aria-live="polite" style={{ color: "green" }}>
+          Result: {result}
+        </p>
+      )}
 
-      <div role='alert' aria-atomic="true">
-        <p>Make sure you enter numbers correctly! Hello</p>
+      <div role="alert" aria-atomic="true">
+        <p>Make sure you enter numbers correctly!</p>
       </div>
     </main>
   );
